3a007b02a53e581fff80cbd275e3a4bcf38545a0
Léa Marie <lea.marie3@live.fr>
2021-12-02 12:01:15 +0100

    mobile corrections

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
 import BeatMachine from './components/BeatMachine';
 import Header from './components/Header';
+import { useEffect } from 'react'
 
 function App() {
+
+  //Corrects 100vh problems on mobile
+  useEffect ( () => {
+    const appHeight = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--fullHeight', `${vh}px`);
+    }
+    window.addEventListener('resize', appHeight)
+    appHeight()
+  }, [])
+
   return (
     <div className="App">
         <div className='container'>
